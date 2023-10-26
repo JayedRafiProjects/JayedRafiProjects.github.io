bc851a20e804105ab9cd3539778e81793bfd53f9
Jayed Rafi <59977352+JayedRafiProjects@users.noreply.github.com>
2023-10-26 11:24:57 -0500

    Add files via upload

--- drmotor/script/data.js
+++ drmotor/script/data.js
@@ -5,6 +5,10 @@ d3.csv("https://jayedrafi.com/drmotor/data/JTHCF1D28E5008692.csv", function(data
 	var licensePlate = data[0].plate;
 	var location = data[0].location;
 	var lastKm = data[data.length-1].odometer;
+	var wheelSize = data[0].wsize;
+	var wheelMaterial = data[0].wmaterial;
+	var wheelSetup = data[0].wsetup;
+	var tire = data[0].tire;
 	
        for (var i = 0; i < data.length; i++) {
         myd+="<tr><td>"+data[i].date+"</td><td>"+data[i].odometer+"</td><td>"+data[i].source+"</td><td>"+data[i].details+"</td></tr>";
@@ -15,4 +19,6 @@ d3.select("#history").html(myd);
 d3.select("#vin").html(vinNumber);
 d3.select("#plate").html(licensePlate+" ("+location+")");
 d3.select("#lastKm").html(lastKm);
+d3.select("#wheel").html(wheelSize+" inch / "+wheelMaterial+" / "+wheelSetup);
+d3.select("#tire").html(tire);
 });
