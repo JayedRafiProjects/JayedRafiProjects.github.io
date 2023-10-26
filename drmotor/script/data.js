6fd634ba959bbfabc83e58213c5a360c959b4a35
Jayed Rafi <59977352+JayedRafiProjects@users.noreply.github.com>
2023-10-26 12:15:31 -0500

    Add files via upload

--- drmotor/script/data.js
+++ drmotor/script/data.js
@@ -14,6 +14,30 @@ d3.csv("https://jayedrafi.com/drmotor/data/JTHCF1D28E5008692.csv", function(data
         myd+="<tr><td>"+data[i].date+"</td><td>"+data[i].odometer+"</td><td>"+data[i].source+"</td><td>"+data[i].details+"</td></tr>";
 
     } 
+	
+	//tire recommendation
+	var winter = "&#x2713";//false
+	var fall = "&#x2713";//false
+	var summer = "&#x2713";//false
+	
+	if(tire=="All weather"){
+		winter = "&#x2717";
+		fall = "&#x2717";
+		summer = "&#x2717";
+	}
+	else if(tire =="Winter"){
+		winter = "&#x2717";
+		
+	}
+	else if(tire == "Summer"){
+		summer = "&#x2717";
+		
+	}
+	else if(tire == "All season"){
+		summer = "&#x2717";
+		fall = "&#x2717";
+	}
+	
 
 d3.select("#history").html(myd);
 d3.select("#vin").html(vinNumber);
@@ -21,4 +45,8 @@ d3.select("#plate").html(licensePlate+" ("+location+")");
 d3.select("#lastKm").html(lastKm);
 d3.select("#wheel").html(wheelSize+" inch / "+wheelMaterial+" / "+wheelSetup);
 d3.select("#tire").html(tire);
+d3.select("#wintertire").html(winter);
+d3.select("#falltire").html(fall);
+d3.select("#summertire").html(summer);
+
 });
